f76235b6cd7429fddf493a7c9e0c6566e030fa5e
tinodin <constantin.schlueter@icloud.com>
2026-03-11 10:00:15 +0100

    Improve UpdateStage

--- Views/Updater/Stages/UpdateStage.cs
+++ Views/Updater/Stages/UpdateStage.cs
@@ -16,23 +16,22 @@ public static class UpdateStage
             ("Disabling new Windows Start Menu Layout", async () => await ProcessActions.RunNsudo("TrustedInstaller", @"reg add ""HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FeatureManagement\Overrides\8\3036241548"" /v ""VariantPayloadKind"" /t REG_DWORD /d 0 /f"), null),
         
             // stop windhawk
-            ("Stopping Windhawk", async () => await ProcessActions.RunNsudo("TrustedInstaller", @"sc stop Windhawk"), null),
             ("Stopping Windhawk", async () => await ProcessActions.RunNsudo("TrustedInstaller", @"cmd /c taskkill /f /im Windhawk.exe"), null),
+            ("Stopping Windhawk", async () => await ProcessActions.RunNsudo("TrustedInstaller", @"sc stop Windhawk"), null),
             ("Stopping Windhawk", async () => await ProcessActions.RunNsudo("TrustedInstaller", @"cmd /c rmdir /s /q ""C:\ProgramData\Windhawk"""), null),
 
             // download windhawk
-            ("Downloading Windhawk", async () => await dialog.Download("https://www.dl.dropboxusercontent.com/scl/fi/omk2gg29v8yguskw4jhng/Windhawk.zip?rlkey=tljvtfus2tq57d3y5mzdt8ges&st=5h7z80ir&dl=0", Path.GetTempPath(), "Windhawk.zip", "Downloading Windhawk", dialog.CurrentGroupStart, dialog.CurrentGroupTarget), null),
+            ("Downloading Windhawk", async () => await dialog.Download("https://www.dl.dropboxusercontent.com/scl/fi/yndylbu9slapalnfvj7p6/Windhawk.zip?rlkey=xhw0ohomb44hxvc28pm80sii2&st=ikti98yr&dl=0", Path.GetTempPath(), "Windhawk.zip", "Downloading Windhawk", dialog.CurrentGroupStart, dialog.CurrentGroupTarget), null),
         
             // extract windawhk
             ("Extracting Windhawk", async () => await ProcessActions.RunExtract(Path.Combine(Path.GetTempPath(), "Windhawk.zip"), Path.Combine(Path.GetTempPath(), "Windhawk")), null),
 
             // update windhawk mods
-            ("Updating Windhawk Mods", async () => await ProcessActions.RunNsudo("TrustedInstaller", @"cmd /c robocopy ""%TEMP%\Windhawk\Windhawk"" ""C:\ProgramData\Windhawk"" /MOVE /E"), null),
+            ("Updating Windhawk Mods", async () => await ProcessActions.RunNsudo("TrustedInstaller", @"cmd /c robocopy ""%TEMP%\Windhawk\Windhawk"" ""C:\ProgramData\Windhawk"" /MIR"), null),
             ("Updating Windhawk Mods", async () => await ProcessActions.RunNsudo("TrustedInstaller", @"reg add ""HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\auto-theme-switcher"" /v LibraryFileName /t REG_SZ /d ""auto-theme-switcher_1.2.0_921892.dll"" /f"), null),
             ("Updating Windhawk Mods", async () => await ProcessActions.RunNsudo("TrustedInstaller", @"reg add ""HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\auto-theme-switcher"" /v Version /t REG_SZ /d ""1.2.0"" /f"), null),
-            ("Updating Windhawk Mods", async () => await ProcessActions.RunPowerShell("Stop-Process -Name explorer -Force"), null),
-            ("Updating Windhawk Mods", async () => await ProcessActions.RunNsudo("TrustedInstaller", @"sc start Windhawk"), null)
-
+            ("Updating Windhawk Mods", async () => await ProcessActions.RunNsudo("TrustedInstaller", @"sc start Windhawk"), null),
+            ("Updating Windhawk Mods", async () => await ProcessActions.RunPowerShell("Stop-Process -Name explorer -Force"), null)
         ];
     }
 }
