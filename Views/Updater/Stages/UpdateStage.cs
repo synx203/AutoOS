2052b0b2b2451cc8f053aa80c47eb11dca5113d0
tinodin <constantin.schlueter@icloud.com>
2026-03-31 12:05:33 +0200

    Change "Heterogeneous short running thread scheduling policy" to "All processors"

--- Views/Updater/Stages/UpdateStage.cs
+++ Views/Updater/Stages/UpdateStage.cs
@@ -1,6 +1,4 @@
-using AutoOS.Helpers.Registry;
 using AutoOS.Views.Settings.Power;
-using Microsoft.Win32;
 
 namespace AutoOS.Views.Updater.Stages;
 
@@ -14,14 +12,9 @@ public static class UpdateStage
         [
             // update power plan
             ("Selecting AutoOS Power Plan", async () => guid = PowerApi.GetPlanGuidByName("AutoOS"), null),
-            (@"Enabling ""Allow Standby States""", async () => PowerApi.WriteACValueIndex(guid, new Guid("238c9fa8-0aad-41ed-83f4-97be242c8f20"), new Guid("abfc2519-3608-4c2a-94ea-171b0ed546ab"), 1), null),
-            (@"Setting ""Processor efficiency containment concurrency threshold"" to 100", async () => PowerApi.WriteACValueIndex(guid, new Guid("54533251-82be-4824-96c1-47b60b740d00"), new Guid("69439b22-221b-4830-bd34-f7bcece24583"), 100), null),
-            (@"Setting ""Processor hybrid containment concurrency threshold"" to 100", async () => PowerApi.WriteACValueIndex(guid, new Guid("54533251-82be-4824-96c1-47b60b740d00"), new Guid("6788488b-1b90-4d11-8fa7-973e470dff47"), 100), null),
-            ("Applying Changes", async () =>  PowerApi.PowerSetActiveScheme(guid), null),
-
-            // optimize raw mouse throttling
-            ("Setting raw mouse throttle duration to 20 ms", async () => RegistryHelper.SetValue(RegistryHelper.Identity.CurrentUser, @"HKEY_CURRENT_USER\Control Panel\Mouse", "RawMouseThrottleDuration", 20, RegistryValueKind.DWord), null),
-            ("Setting raw mouse throttle leeway to 0ms", async () => RegistryHelper.SetValue(RegistryHelper.Identity.CurrentUser, @"HKEY_CURRENT_USER\Control Panel\Mouse", "RawMouseThrottleLeeway", 0, RegistryValueKind.DWord), null)
+            (@"Setting ""Heterogeneous short running thread scheduling policy"" to ""All processors""", async () => PowerApi.WriteACValueIndex(guid, new Guid("54533251-82be-4824-96c1-47b60b740d00"), new Guid("bae08b81-2d5e-4688-ad6a-13243356654b"), 0), null),
+            (@"Setting ""Heterogeneous short running thread scheduling policy"" to ""All processors""", async () => PowerApi.WriteDCValueIndex(guid, new Guid("54533251-82be-4824-96c1-47b60b740d00"), new Guid("bae08b81-2d5e-4688-ad6a-13243356654b"), 0), null),
+            ("Applying Changes", async () =>  PowerApi.PowerSetActiveScheme(guid), null)
         ];
     }
 }
