cef4972f6d7f8856ec946d002fbebf6c73d0c0e4
tinodin <constantin.schlueter@icloud.com>
2026-04-21 14:43:57 +0200

    Bump version

--- Views/Updater/Stages/UpdateStage.cs
+++ Views/Updater/Stages/UpdateStage.cs
@@ -1,7 +1,3 @@
-using AutoOS.Helpers.Registry;
-using AutoOS.Helpers.GPU;
-using Microsoft.Win32;
-
 namespace AutoOS.Views.Updater.Stages;
 
 public static class UpdateStage
@@ -9,15 +5,6 @@ public static class UpdateStage
     public static List<(string Title, Func<Task> Action, Func<bool> Condition)> UpdateActions(UpdateDialog dialog)
     {
         var actions = new List<(string Title, Func<Task> Action, Func<bool> Condition)>();
-
-        var gpus = GpuHelper.GetGPUs().Where(gpu => gpu.NVIDIA);
-
-        foreach (var gpu in gpus)
-        {
-            // force "hardware composed: independent flip"
-            actions.Add((@"Forcing ""Hardware Composed: Independent Flip""", async () => RegistryHelper.SetValue(RegistryHelper.Identity.TrustedInstaller, gpu.RegistryPath, "enableRS2FlipCollapse", 1, RegistryValueKind.DWord), null));
-            actions.Add((@"Forcing ""Hardware Composed: Independent Flip""", async () => RegistryHelper.SetValue(RegistryHelper.Identity.TrustedInstaller, gpu.RegistryPath, "enableRS2ImmediateFlipCompletionReporting", 1, RegistryValueKind.DWord), null));
-        }
 
         return actions;
     }
