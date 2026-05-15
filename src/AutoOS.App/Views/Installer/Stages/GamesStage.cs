536499c52618303ff09c08ef31945e66f2780033
tinodin <constantin.schlueter@icloud.com>
2026-05-15 22:37:42 +0200

    Fix GamesStage.cs

--- src/AutoOS.App/Views/Installer/Stages/GamesStage.cs
+++ src/AutoOS.App/Views/Installer/Stages/GamesStage.cs
@@ -24,17 +24,13 @@ public static partial class GamesStage
 		int maxRefreshRate = (int)MonitorHelper.GetMonitors().Max(max => max.RefreshRate);
 
         var actions = new List<(string Title, Func<Task> Action, Func<bool> Condition)>
-        {
-            // install easyanticheat
-            ("Installing EasyAntiCheat", async () => await Process.Start(new ProcessStartInfo($@"{fortnitePath}\FortniteGame\Binaries\Win64\EasyAntiCheat\EasyAntiCheat_EOS_Setup.exe", "install 4fe75bbc5a674f4f9b356b5c90567da5") {  WindowStyle = ProcessWindowStyle.Hidden })!.WaitForExitAsync(), () => Fortnite == true),
-            ("Installing EasyAntiCheat", async () => await Task.Delay(1000), () => Fortnite == true),
-        
+        {        
             // download gameusersettings.ini for fortnite
             ("Downloading GameUserSettings.ini for Fortnite", async () => await DownloadHelper.Download("https://www.dl.dropboxusercontent.com/scl/fi/x7ymbpu9hf6myle0an2ef/GameUserSettings.ini?rlkey=i9v5oc1nccx7k58g12dd1k33j&st=hwo4v2du&dl=0", fortniteIniPath, "GameUserSettings.ini"), () => Fortnite == true),
             
             // cap frame rate for fortnite
-            ($"Capping Frame Rate for Fortnite to {maxRefreshRate}", async () => new InIHelper(Path.Combine(fortniteIniPath, "GameUserSettings.ini")).AddValue("FrameRateLimit", $"{maxRefreshRate}.000000", "/Script/FortniteGame.FortGameUserSettings"), () => Fortnite == true),
-            ($"Capping Frame Rate for Fortnite to {maxRefreshRate}", async () => await Task.Delay(1000), () => Fortnite == true),
+            ($"Capping Frame Rate for Fortnite to {maxRefreshRate}fps", async () => new InIHelper(Path.Combine(fortniteIniPath, "GameUserSettings.ini")).AddValue("FrameRateLimit", $"{maxRefreshRate}.000000", "/Script/FortniteGame.FortGameUserSettings"), () => Fortnite == true),
+            ($"Capping Frame Rate for Fortnite to {maxRefreshRate}fps", async () => await Task.Delay(1000), () => Fortnite == true),
 
             // disable fullscreen optimizations for fortnite
             ("Disabling fullscreen optimizations for Fortnite", async () => RegistryHelper.SetValue(RegistryHelper.Identity.CurrentUser, @"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers", $@"{fortnitePath}\FortniteGame\Binaries\Win64\FortniteClient-Win64-Shipping.exe", "~ DISABLEDXMAXIMIZEDWINDOWEDMODE", RegistryValueKind.String), () => Fortnite == true),
@@ -44,12 +40,16 @@ public static partial class GamesStage
             (@"Setting ""GPU Preference"" to ""High Performance"" for Fortnite", async () => RegistryHelper.SetValue(RegistryHelper.Identity.CurrentUser, @"HKEY_CURRENT_USER\Software\Microsoft\DirectX\UserGpuPreferences", fortnitePath + @"\FortniteGame\Binaries\Win64\FortniteClient-Win64-Shipping.exe", "SwapEffectUpgradeEnable=1;GpuPreference=2;", RegistryValueKind.String), () => Fortnite == true),
             (@"Setting ""GPU Preference"" to ""High Performance"" for Fortnite", async () => await Task.Delay(1000), () => Fortnite == true),
 
+			// install easyanticheat
+            ("Installing EasyAntiCheat", async () => await Process.Start(new ProcessStartInfo($@"{fortnitePath}\FortniteGame\Binaries\Win64\EasyAntiCheat\EasyAntiCheat_EOS_Setup.exe", "install 4fe75bbc5a674f4f9b356b5c90567da5") {  WindowStyle = ProcessWindowStyle.Hidden })!.WaitForExitAsync(), () => Fortnite == true),
+			("Installing EasyAntiCheat", async () => await Task.Delay(1000), () => Fortnite == true),
+
             // download gameusersettings.ini for valorant
             ("Downloading GameUserSettings.ini for Valorant", async () => await DownloadHelper.Download("https://www.dl.dropboxusercontent.com/scl/fi/v8t7zr92smdwp6c0u43li/GameUserSettings.ini?rlkey=9utj5hcekf4ddvpvvxbzzhbua&st=y7q4r3hm&dl=0", valorantIniPath, "GameUserSettings.ini"), () => Valorant == true),
             
             // cap frame rate for valorant
-            ($"Capping Frame Rate for Valorant to {maxRefreshRate}", async () => new InIHelper(Path.Combine(valorantIniPath, "GameUserSettings.ini")).AddValue("FrameRateLimit", $"{maxRefreshRate}.000000", "/Script/FortniteGame.FortGameUserSettings"), () => Valorant == true),
-            ($"Capping Frame Rate for Valorant to {maxRefreshRate}", async () => await Task.Delay(1000), () => Valorant == true),
+            ($"Capping Frame Rate for Valorant to {maxRefreshRate}fps", async () => new InIHelper(Path.Combine(valorantIniPath, "GameUserSettings.ini")).AddValue("FrameRateLimit", $"{maxRefreshRate}.000000", "/Script/FortniteGame.FortGameUserSettings"), () => Valorant == true),
+            ($"Capping Frame Rate for Valorant to {maxRefreshRate}fps", async () => await Task.Delay(1000), () => Valorant == true),
 
             // // set "gpu preference" to "high performance" for valorant
             // (@"Setting ""GPU Preference"" to ""High Performance"" for Valorant", async () => valorantPath = , () => Valorant == true),
